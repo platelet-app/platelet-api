8259c56a4eded51cde7f09d5959dc70d37507295
Theo Cranmore <theo.cranmore@gmail.com>
2018-11-16 20:17:35 +0000

    Vehicle and User models

--- app/models.py
+++ app/models.py
@@ -12,6 +12,10 @@ class Rider(db.Model):
     dob = db.Column(db.Date)
     status = db.Column(db.String(64))
     assignedVehicle = db.Column(db.Integer)
+    patch = db.Column(db.String(64))
+
+    def __repr__(self):
+        return '<Rider {} in patch {} with vehicle {}>'.format(self.name, self.patch, self.assignedVehicle)
 
 
 class Task(db.Model):
@@ -30,3 +34,27 @@ class Task(db.Model):
     finalDuration = db.Column(db.Time)
     miles = db.Column(db.Integer)
 
+    def __repr__(self):
+        return '<Task ID {} taken at {} with priority {}>'.format(str(self.id), str(self.timestamp), str(self.priority))
+
+
+class Vehicle(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    manufacturer = db.Column(db.String(64))
+    model = db.Column(db.String(64))
+    dateOfManufacture = db.Column(db.Date)
+    dateOfRegistration = db.Column(db.Date)
+    registrationNumber = db.Column(db.String(10))
+
+    def __repr__(self):
+        return '<Vehicle {} {} with registration {}>'.format(self.manufacturer, self.model, self.registrationNumber)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    passwordHash = db.Column(db.String(128))
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
