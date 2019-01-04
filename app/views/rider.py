7d8d537aaca3b11fc40ac7dc06c08abfec663722
Theo Cranmore <theo.cranmore@gmail.com>
2019-01-04 23:05:41 +0000

    Added post function for adding a user of type rider. Get function also returns rider information.

--- app/views/rider.py
+++ app/views/rider.py
@@ -1,12 +1,21 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, request
 from flask import jsonify
 from app import models
 from flask_restful import reqparse, abort, Api, Resource
 from app import db
 from app import api
 
+from datetime import datetime
+
 parser = reqparse.RequestParser()
-parser.add_argument('rider')
+#parser.add_argument('name', 'username', 'email', 'dob', 'status', 'assignedVehicle', 'patch')
+parser.add_argument('name')
+parser.add_argument('username')
+parser.add_argument('email')
+parser.add_argument('dob')
+parser.add_argument('status')
+parser.add_argument('vehicle')
+parser.add_argument('patch')
 
 mod = Blueprint('rider', __name__, url_prefix='/api/v1/rider/')
 
@@ -19,19 +28,32 @@ class Rider(Resource):
 
         if (rider):
             return { 'id': rider.id, 'name': rider.name, 'patch': rider.patch,
-                           'dob': rider.dob, 'vehicle': rider.assignedVehicle, 'status': rider.status,
+                           'dob': datetime.strftime(rider.dob, '%d/%m/%Y'), 'vehicle': rider.assignedVehicle, 'status': rider.status,
                            'address1': rider.address1, 'address2': rider.address2,
                            'town': rider.town, 'county': rider.county,
                            'postcode': rider.postcode, 'country': rider.country }
         else:
             return notFound()
 
+class RiderCreator(Resource):
 
-    def put(self, id):
+    def post(self):
+
         args = parser.parse_args()
+        for i in args:
+            print(i)
+            
+        rider = models.Rider(name=args['name'], email=args['email'], dob=datetime.strptime(args['dob'], '%d/%m/%Y'),\
+                             status=args['status'], assignedVehicle=int(args['vehicle']),\
+                             patch=args['patch'])
+        db.session.add(rider)
+        db.session.commit()
 
+        return rider.id, 201
+    
 
 api.add_resource(Rider, '/rider/<id>')
+api.add_resource(RiderCreator, '/rider/')
 
 @mod.route('<int:id>/name', methods=['GET'])
 def getRiderName(id):
@@ -90,8 +112,12 @@ def getRiderPatch(id):
         return notFound()
 
 
-@mod.route('submit', methods=['PUT'])
+@mod.route('submit', methods=['POST'])
 def saveRider():
+
+    for i in request.form:
+        print(i)
+
     return "saved... lol not really"
 
 
