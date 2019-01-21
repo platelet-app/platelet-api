ce9016bd578b835bc751dd18d15d0c7fd0df87e5
Theo <theo.cranmore@gmail.com>
2019-01-21 22:20:53 +0000

    created separate view function files for each model and added delete function/test for session

--- app/views/functions/viewfunctions.py
+++ app/views/functions/viewfunctions.py
@@ -4,22 +4,30 @@ import functools
 from flask_praetorian import utilities
 from flask import request
 import json
-
-
-def notFound(id = "null"):
-    return {'id': id, 'message': "The user was not found"}, 404
+from app import db
+from app import models
 
 
 def databaseError(id = "null"):
     traceback.print_exception(*sys.exc_info())
-    return {'id': id, 'message': "A database error has occurred"}, 500
+    return {'id': id, 'message': "a database error has occurred"}, 500
 
 
 def notUniqueError(field, id = "null"):
     return {'id': id, 'message': "{} not unique".format(field)}, 403
 
+
 def unauthorisedError(message):
     return {'message': message}, 401
+
+
+def forbiddenError(message):
+    return {'message': message}, 403
+
+
+def notFound(what, id = "null"):
+    return {'id': id, 'message': "The {} was not found".format(what)}, 404
+
 
 def userIdMatchOrAdmin(func):
     @functools.wraps(func)
@@ -31,6 +39,19 @@ def userIdMatchOrAdmin(func):
         else:
             return {"id": _id, "message": "Object not owned by user"}, 401
     return wrapper
+
+
+def sessionIdMatchOrAdmin(func):
+    @functools.wraps(func)
+    def wrapper(self, _id):
+        if 'admin' in utilities.current_rolenames():
+            return func(self, _id)
+        if models.Session.query.filter_by(id=_id).first().user_id == utilities.current_user_id():
+            return func(self, _id)
+        else:
+            return {"id": _id, "message": "Object not owned by user"}, 401
+    return wrapper
+
 
 def loadRequestIntoObject(schema, objectToLoadInto):
     requestJson = request.get_json()
@@ -44,3 +65,6 @@ def loadRequestIntoObject(schema, objectToLoadInto):
     objectToLoadInto.updateFromDict(**parsedSchema.data)
 
     return None
+
+def getAllUsers():
+    return models.User.query.all()
