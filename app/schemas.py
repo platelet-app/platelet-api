d8786e7fb3f8359e2c3bf18f226c502f3037bcab
Theo <theo.cranmore@gmail.com>
2020-04-18 05:18:41 +0100

    removed comments field from CommentsSchema

--- app/schemas.py
+++ app/schemas.py
@@ -27,7 +27,7 @@ class CommentSchema(ma.ModelSchema, TimesMixin):
     class Meta:
         model = models.Comment
         fields = ('uuid', 'body', 'author', 'parent_uuid', 'author_uuid',
-                  "time_created", "time_modified", "publicly_visible", "comments")
+                  "time_created", "time_modified", "publicly_visible")
     author = fields.fields.Nested('UserSchema', dump_only=True, exclude=('username', 'address', 'password', 'name', 'email',
                   'dob', 'patch', 'roles', 'comments', 'assigned_vehicles', 'patch_id',
                   "time_created", "time_modified"))
