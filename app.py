cc5237efd510fbc68233601aa9d7823c102caf62
Theo Cranmore <theo.cranmore@gmail.com>
2021-08-13 16:26:38 +0100

    allow host to be set as argument to app.py

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
 from app import app
 from app.api.sockets import socketio
+import argparse
+
+parser = argparse.ArgumentParser(description='app root')
+parser.add_argument('--host', dest='host', type=str)
+
+args = parser.parse_args()
 
 if __name__ == '__main__':
-    socketio.socketIO.run(app)
+    socketio.socketIO.run(app, host=args.host)
